5b4f4eda0294644727f5626a953bc9e534f7192a
codyjgeorge <codygeorge315@gmail.com>
2025-07-30 13:48:39 -0500

    front end bug fixes

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,8 @@ app.use(express.json());
 
 // Options Handler for MonkeyType API
 app.options('/api/monkeytype', (req, res) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || 'https://codyjgeorge.github.io/Simple_Hacker_Portfolio/');
+    console.log('OPTIONS request received for /api/monkeytype');
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
@@ -48,7 +49,7 @@ app.options('/api/monkeytype', (req, res) => {
 // Proxy endpoint (POST Handler) for MonkeyType API
 app.post('/api/monkeytype', async (req, res) => {
     // Set CORS headers explicitly
-    res.header('Access-Control-Allow-Origin', req.headers.origin || 'https://codyjgeorge.github.io/Simple_Hacker_Portfolio/');
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
     res.header('Access-Control-Allow-Credentials', 'true');
